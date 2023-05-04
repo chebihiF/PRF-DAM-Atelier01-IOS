c8c4b8832d5b7038279cda25de81dc2a8336b429
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-05-04 08:42:00 +0100

    Initial Commit

---
+++ Atelier01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Atelier01
+//
+//  Created by CHEBIHI FAYCAL on 4/5/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
