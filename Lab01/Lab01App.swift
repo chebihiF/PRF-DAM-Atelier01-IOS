5f10aadea476bd364536609b5ef43665c7c23171
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-05-05 10:36:41 +0100

    Initial Commit

---
+++ Lab01/Lab01App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab01App.swift
+//  Lab01
+//
+//  Created by CHEBIHI FAYCAL on 5/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct Lab01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
